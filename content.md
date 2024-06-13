7e7e86c1819d3611a4af3b7689278588e0c2624d
Otarossoni <otarossoni@gmail.com>
2024-06-13 20:55:32 -0300

    feat: estrutura do projeto

---
+++ content.md
@@ -0,0 +1,15 @@
+<div id="header">
+   <p align="center">
+   <h1 align="center">Kernel</h1>
+   </p>
+</div>
+
+Aqui é um pequeno lugar da internet que reúne tudo feito por [Otávio Monteiro Rossoni](https://github.com/Otarossoni), como projetos, estudos e algoritmos. 
+
+### Contato
+
+- [E-mail](mailto:otarossoni@gmail.com)
+- [Github](https://github.com/Otarossoni)
+- [LinkedIn](https://www.linkedin.com/in/otavio-monteiro-rossoni/)
+- [Twitter / X](https://x.com/Otarossoni)
+- [Instagram](https://www.instagram.com/otarossoni/)
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const fetchContainerContent = function () {
+  fetch('https://raw.githubusercontent.com/Otarossoni/kernel/main/content.md')
+      .then(response => response.text())
+      .then(data => {
+        const readmeHtml = marked(data);
+        const container = document.getElementById('content');
+        container.innerHTML = readmeHtml;
+        Prism.highlightAllUnder(container);
+      })
+      .catch(error => console.error("Error:", error));
+}
+document.addEventListener("DOMContentLoaded", fetchContainerContent());
